08aa5ce2c42ec754f5cc1a2c443d4e968af5ba38
iCOnicHaVOc <adityaniser2328@gmail.com>
2025-10-03 20:35:54 +0530

    improve the output

--- assign_9.py
+++ assign_9.py
@@ -5,19 +5,16 @@ from My2lib import *
 Eq1 = [1,-1,-7,1,6]
 Eq2 = [1,0,-5,0,4]
 Eq3 = [2.0,0.0,-19.5,0.5,13.5,-4.5]
-def fun1(x):
-    return x**4 -x**3- 7*x**2 +x +6
-def fun2(x):
-    return x**4 -5*x**2 + 4
-def fun3(x):
-    return 2.0*x**5 - 19.5*x**3 + 0.5*x**2 + 13.5*x - 4.5
 
- 
-initial_guesses = [3,1,-2,-1]  
-all_roots = find_all_roots(fun1, Eq1, initial_guesses)
 
-initial_guesses = [-2,2,-1,1]  
-all_roots = find_all_roots(fun2, Eq2, initial_guesses)
+guess = [-1.9,-0.2,1.1,3.1]  
+all_roots, steps = lagurre(Eq1, guess, 20, 1e-6, 1e-6, synthetic_division, differentiation, fun_builder)
+print("By Lagure's method - The roots of the function are", all_roots, "reached in", steps, "respective steps")
 
-initial_guesses = [0,0,1.85,0,0]  
-all_roots = find_all_roots(fun3, Eq3, initial_guesses)
+guess2 = [-1.9,-0.8,1.1,6]
+all_roots2, steps2 = lagurre(Eq2, guess2, 20, 1e-6, 1e-6, synthetic_division, differentiation, fun_builder)
+print("By Lagure's method - The roots of the function are", all_roots2, "reached in", steps2, "respective steps")
+
+guess3 = [-2,0,2,5,6]
+all_roots3, steps3 = lagurre(Eq3, guess3, 30, 1e-6, 1e-6, synthetic_division, differentiation, fun_builder)
+print("By Lagure's method - The roots of the function are", all_roots3, "reached in", steps3, "respective steps")   
