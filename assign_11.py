9a25a80e04ea1237b00d868a14ba9f82ba29477b
iCOnicHaVOc <adityaniser2328@gmail.com>
2025-10-09 18:48:50 +0530

    printed midpoint solution

--- assign_11.py
+++ assign_11.py
@@ -14,9 +14,13 @@ def f3(x):
 
 int1 = int_simp(f1, 1, 2, 24)
 print("The value of function 1 using simpson's rule with N= 28 is", int1)
+mid = midpoint_integration(f1, 1, 2, 10)
+print("The value of function 1 using midpoint rule with N= 10 is", mid)
 
 int = int_simp(f2, 0, math.pi/2, 28)
 print("The value of function 2 using simpson's rule with N= 28 is", int)
+mid2 = midpoint_integration(f2, 0, math.pi/2, 10)
+print("The value of function 2 using midpoint rule with N= 10 is", mid2)
 
 o,p,q,r,t = monte_carlo_int(f3,-1,1,100,LCG,123456,1e-3)
 print("The value of the integral using monte carlo accurate upto 4 decimal place is", o, "with standard deviation", p)
@@ -37,7 +41,9 @@ Plot(q,t,
 # ======================MY SOLUTION=========================
 '''
 The value of function 1 using simpson's rule with N= 28 is 0.6931472743443177
+The value of function 1 using midpoint rule with N= 10 is 0.6928353604099603
 The value of function 2 using simpson's rule with N= 28 is 0.5707965784470774
+The value of function 2 using midpoint rule with N= 10 is 0.573442705787416
 Increasing number of points to 200
 Increasing number of points to 400
 Increasing number of points to 800
